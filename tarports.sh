eb502eb42df555ae7149b98448c78e5df6ab6a86
Nicholas Howland <94145193+nicholas-howland@users.noreply.github.com>
2026-02-08 13:49:14 -0800

    Update tarports.sh

--- tarports.sh
+++ tarports.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 iptables -t nat -A PREROUTING -p tcp -m tcp --dport 1:65535 -j REDIRECT --to-ports 8000
-gcc bonk.c -o bonk
-nc -tlv -n -e ./bonk -p 8000
+# /dev/random may also be able to be used here, but we know that the following will work
+gcc flood-terminal.c -o flood-terminal
+nc -tlv -n -e ./flood-terminal -p 8000
